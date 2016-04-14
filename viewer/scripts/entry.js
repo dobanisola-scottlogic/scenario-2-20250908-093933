061f7c32955d32199292af66cef63b4c70019ca7
Robert McNeill <rjmcneill@users.noreply.github.com>
2016-04-14 16:47:02 +0100

    HACK-19 - Added preprocessing of JSON game object

--- viewer/scripts/entry.js
+++ viewer/scripts/entry.js
@@ -2,3 +2,8 @@ require('../styles/style.css');
 require('pixi');
 require('p2');
 require('phaser');
+
+var gameData = require('json!../testData/exampleGame');
+var parser = require('./parser');
+
+var parsedJson = parser(gameData);
---
+++ viewer/scripts/models/Cell.js
@@ -0,0 +1,8 @@
+class Cell {
+    constructor(col, row) {
+        this.col = col;
+        this.row = row;
+    }
+}
+
+module.exports = Cell;
