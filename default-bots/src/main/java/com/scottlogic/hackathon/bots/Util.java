dobanisola-scottlogic/scenario-2-20250908-135729

commit 7fb11e22e7672a8d4c41b47470c05145a335f724

Update default-bots to make use of utility methods in GameState and Map

default-bots/src/main/java/com/scottlogic/hackathon/bots/Util.java
@@ -3,14 +3,14 @@ package com.scottlogic.hackathon.bots;
 import com.scottlogic.hackathon.game.Direction;
 import com.scottlogic.hackathon.game.Position;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Random;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class Util {
 
-    public Util(final int mapWidth, final int mapHeight) {
+    public Util(int mapWidth, int mapHeight) {
         this.mapWidth = mapWidth;
         this.mapHeight = mapHeight;
         this.xDirectionModifiers = createXDirectionModifiersMap();
@@ -85,34 +85,6 @@ public class Util {
         return modifiedY;
     }
 
-    public Direction findBestDirectionFirstPositionToAnother(Position myPosition, Position desiredPosition) {
-        int xDifference = getMinimumXDifference(desiredPosition.getX(), myPosition.getX());
-        int yDifference = getMinimumYDifference(desiredPosition.getY(), myPosition.getY());
-        // returns radian angle between -pi and +pi
-        // y decrements as we go north REMEMBER
-        double theta = Math.atan2((double) -yDifference, (double) xDifference);
-        // HARD CODED Method BASED ON THE 8 DIRECTION VALUES (
-        int modifiedTheta = (int) Math.round(2 - 4 * theta / Math.PI);
-        if (modifiedTheta < 0) {
-            modifiedTheta += directions.length;
-        }
-        return directions[modifiedTheta];
-    }
-
-    public Direction findBestDirectionFirstPositionFromAnother(Position myPosition, Position avoidedPosition) {
-        int xDifference = getMinimumXDifference(avoidedPosition.getX(), myPosition.getX());
-        int yDifference = getMinimumYDifference(avoidedPosition.getY(), myPosition.getY());
-        // returns radian angle between -pi and +pi
-        // y decrements as we go north REMEMBER
-        double theta = Math.atan2((double) -yDifference, (double) xDifference);
-        // HARD CODED Method BASED ON THE 8 DIRECTION VALUES (
-        int modifiedTheta = (int) Math.round(6 - 4 * theta / Math.PI);
-        if (modifiedTheta >= directions.length) {
-            modifiedTheta -= directions.length;
-        }
-        return directions[modifiedTheta];
-    }
-
     public Direction findBestTetherDirectionOnePositionToAnother(Position myPosition, Position desiredTether, int distance) {
         int minDistanceToTether = distance * distance;
         int index = new Random().nextInt(directions.length);
@@ -138,17 +110,12 @@ public class Util {
         return new Random().nextInt(maxDistance - minDistance) + minDistance;
     }
 
-    public Direction getOppositeDirection(Direction direction) {
-        int directionIndex = Arrays.asList(directions).indexOf(direction);
-        directionIndex += directions.length / 2;
-        if (directionIndex >= directions.length) {
-            directionIndex -= directions.length;
-        }
-        return directions[directionIndex];
-    }
-
     public Direction randomDirection() {
         return directions[new Random().nextInt(directions.length)];
+    }
+
+    public Stream<Direction> randomDirections() {
+        return Stream.generate(this::randomDirection);
     }
 
     public boolean playersCollideInThisMove(int distance, Direction direction, Position playerPosition, Set<Position> myPlayersPositions) {

default-bots/src/main/java/com/scottlogic/hackathon/bots/move/AttackMove.java
@@ -1,17 +1,20 @@
 package com.scottlogic.hackathon.bots.move;
 
 import com.scottlogic.hackathon.game.Direction;
+import com.scottlogic.hackathon.game.Map;
 import com.scottlogic.hackathon.game.Player;
 import com.scottlogic.hackathon.game.Position;
 
+import java.util.stream.Stream;
+
 public class AttackMove extends MoveBase {
 
-    public AttackMove(final int mapWidth, final int mapHeight, final Player fullPlayer) {
-        super(mapWidth, mapHeight, fullPlayer);
+    public AttackMove(final Map map, final Player fullPlayer) {
+        super(map, fullPlayer);
     }
 
-    public AttackMove(Direction direction, int distance, final int mapWidth, final int mapHeight, final Player fullPlayer) {
-        super(direction, distance, mapWidth, mapHeight, fullPlayer);
+    public AttackMove(Direction direction, int distance, Map map, final Player fullPlayer) {
+        super(direction, distance, map, fullPlayer);
     }
 
     @Override
@@ -27,10 +30,10 @@ public class AttackMove extends MoveBase {
             }
             if (nearestOpponentPosition != null) {
                 distance = 0;
-                direction = util.findBestDirectionFirstPositionToAnother(playerPosition, nearestOpponentPosition);
-                while (util.playersCollideInThisMove(2, direction, playerPosition, playersPositions)) {
-                    direction = util.randomDirection();
-                }
+                direction = Stream.concat(getMap().directionsTowards(playerPosition, nearestOpponentPosition), util.randomDirections())
+                        .filter(d -> util.playersCollideInThisMove(2, d, playerPosition, playersPositions))
+                        .findFirst()
+                        .get(); // Guaranteed to be present
             } else {
                 setRandomDirectionAndDistance(MINIMUM_RANDOM_DISTANCE, MAXIMUM_RANDOM_DISTANCE);
             }

default-bots/src/main/java/com/scottlogic/hackathon/bots/move/CollectableMove.java
@@ -2,16 +2,19 @@ package com.scottlogic.hackathon.bots.move;
 
 import com.scottlogic.hackathon.game.Collectable;
 import com.scottlogic.hackathon.game.Direction;
+import com.scottlogic.hackathon.game.Map;
 import com.scottlogic.hackathon.game.Player;
+
+import java.util.stream.Stream;
 
 public class CollectableMove extends MoveBase {
 
-    public CollectableMove(final int mapWidth, final int mapHeight, final Player fullPlayer) {
-        super(mapWidth, mapHeight, fullPlayer);
+    public CollectableMove(Map map, final Player fullPlayer) {
+        super(map, fullPlayer);
     }
 
-    public CollectableMove(Direction direction, int distance, final int mapWidth, final int mapHeight, final Player fullPlayer) {
-        super(direction, distance, mapWidth, mapHeight, fullPlayer);
+    public CollectableMove(Direction direction, int distance, Map map, final Player fullPlayer) {
+        super(direction, distance, map, fullPlayer);
     }
 
     @Override
@@ -27,10 +30,10 @@ public class CollectableMove extends MoveBase {
             }
             if (nearestCollectable != null) {
                 distance = 0;
-                direction = util.findBestDirectionFirstPositionToAnother(playerPosition, nearestCollectable.getPosition());
-                while (util.playersCollideInThisMove(2, direction, playerPosition, playersPositions)) {
-                    direction = util.randomDirection();
-                }
+                direction = Stream.concat(getMap().directionsTowards(playerPosition, nearestCollectable.getPosition()), util.randomDirections())
+                        .filter(d -> util.playersCollideInThisMove(2, d, playerPosition, playersPositions))
+                        .findFirst()
+                        .get();
             } else {
                 setRandomDirectionAndDistance(MINIMUM_RANDOM_DISTANCE, MAXIMUM_RANDOM_DISTANCE);
             }

default-bots/src/main/java/com/scottlogic/hackathon/bots/move/HunterMove.java
@@ -1,24 +1,30 @@
 package com.scottlogic.hackathon.bots.move;
 
 import com.scottlogic.hackathon.game.Direction;
+import com.scottlogic.hackathon.game.Map;
 import com.scottlogic.hackathon.game.Player;
 import com.scottlogic.hackathon.game.Position;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class HunterMove extends MoveBase {
 
     private int largestMapRadius;
 
-    public HunterMove(final int mapWidth, final int mapHeight, final Player fullPlayer) {
-        super(mapWidth, mapHeight, fullPlayer);
-        this.largestMapRadius = (int) Math.pow(Math.max(mapWidth, mapHeight), 2);
+    public HunterMove(Map map, final Player fullPlayer) {
+        super(map, fullPlayer);
+        this.largestMapRadius = determineLargestMapRadius(map);
     }
 
-    public HunterMove(Direction direction, int distance, final int mapWidth, final int mapHeight, final Player fullPlayer) {
-        super(direction, distance, mapWidth, mapHeight, fullPlayer);
-        this.largestMapRadius = (int) Math.pow(Math.max(mapWidth, mapHeight), 2);
+    public HunterMove(Direction direction, int distance, Map map, final Player fullPlayer) {
+        super(direction, distance, map, fullPlayer);
+        this.largestMapRadius = determineLargestMapRadius(map);
+    }
+
+    private static int determineLargestMapRadius(Map map) {
+        return (int) Math.pow(Math.max(map.getWidth(), map.getHeight()), 2);
     }
 
     @Override
@@ -38,10 +44,10 @@ public class HunterMove extends MoveBase {
             }
             if (nearestOpponentSpawnPointPosition != null) {
                 distance = 0;
-                direction = util.findBestDirectionFirstPositionToAnother(playerPosition, nearestOpponentSpawnPointPosition);
-                while (util.playersCollideInThisMove(2, direction, playerPosition, playersPositions)) {
-                    direction = util.randomDirection();
-                }
+                direction = Stream.concat(getMap().directionsTowards(playerPosition, nearestOpponentSpawnPointPosition), util.randomDirections())
+                        .filter(d -> util.playersCollideInThisMove(2, d, playerPosition, playersPositions))
+                        .findFirst()
+                        .get();
             } else {
                 setRandomDirectionAndDistance(MINIMUM_RANDOM_DISTANCE, MAXIMUM_RANDOM_DISTANCE);
             }

default-bots/src/main/java/com/scottlogic/hackathon/bots/move/MoveBase.java
@@ -23,26 +23,29 @@ public class MoveBase implements Move {
     Set<SpawnPoint> opponentSpawnPoints = new HashSet<>();
     Set<Collectable> collectables;
 
+    private final Map map;
     Util util;
 
     static final int MINIMUM_RANDOM_DISTANCE = 16;
     static final int MAXIMUM_RANDOM_DISTANCE = 64;
     static final int ACTIVE_RADIUS_SQUARED = 50;
 
-    public MoveBase(final int mapWidth, final int mapHeight, final Player fullPlayer) {
+    public MoveBase(Map map, final Player fullPlayer) {
         this.owner = fullPlayer.getOwner();
         this.playerPosition = fullPlayer.getPosition();
         this.player = fullPlayer.getId();
-        this.util = new Util(mapWidth, mapHeight);
+        this.map = map;
+        this.util = new Util(map.getWidth(), map.getHeight());
     }
 
-    public MoveBase(Direction direction, int distance, final int mapWidth, final int mapHeight, final Player fullPlayer) {
+    public MoveBase(Direction direction, int distance, Map map, final Player fullPlayer) {
+        this(map, fullPlayer);
         this.direction = direction;
         this.distance = distance;
-        this.owner = fullPlayer.getOwner();
-        this.playerPosition = fullPlayer.getPosition();
-        this.player = fullPlayer.getId();
-        this.util = new Util(mapWidth, mapHeight);
+    }
+
+    protected Map getMap() {
+        return map;
     }
 
     public UUID getPlayer() {

default-bots/src/main/java/com/scottlogic/hackathon/bots/move/TimidMove.java
@@ -1,17 +1,20 @@
 package com.scottlogic.hackathon.bots.move;
 
 import com.scottlogic.hackathon.game.Direction;
+import com.scottlogic.hackathon.game.Map;
 import com.scottlogic.hackathon.game.Player;
 import com.scottlogic.hackathon.game.Position;
 
+import java.util.stream.Stream;
+
 public class TimidMove extends MoveBase {
 
-    public TimidMove(final int mapWidth, final int mapHeight, final Player fullPlayer) {
-        super(mapWidth, mapHeight, fullPlayer);
+    public TimidMove(Map map, final Player fullPlayer) {
+        super(map, fullPlayer);
     }
 
-    public TimidMove(Direction direction, int distance, final int mapWidth, final int mapHeight, final Player fullPlayer) {
-        super(direction, distance, mapWidth, mapHeight, fullPlayer);
+    public TimidMove(Direction direction, int distance, Map map, final Player fullPlayer) {
+        super(direction, distance, map, fullPlayer);
     }
 
     @Override
@@ -27,10 +30,10 @@ public class TimidMove extends MoveBase {
             }
             if (nearestOpponentPosition != null) {
                 distance = 0;
-                direction = util.findBestDirectionFirstPositionFromAnother(playerPosition, nearestOpponentPosition);
-                while (util.playersCollideInThisMove(2, direction, playerPosition, playersPositions)) {
-                    direction = util.randomDirection();
-                }
+                direction = Stream.concat(getMap().directionsAwayFrom(playerPosition, nearestOpponentPosition), util.randomDirections())
+                        .filter(d -> util.playersCollideInThisMove(2, d, playerPosition, playersPositions))
+                        .findFirst()
+                        .get();
             } else {
                 setRandomDirectionAndDistance(MINIMUM_RANDOM_DISTANCE, MAXIMUM_RANDOM_DISTANCE);
             }

default-bots/src/main/java/com/scottlogic/hackathon/bots/Milestone1Bot.java
@@ -15,9 +15,6 @@ public class Milestone1Bot extends Bot {
             moves.removeIf(move -> move.getPlayer().equals(player.getId()));
         });
 
-        final int mapWidth = gameState.getMap().getWidth();
-        final int mapHeight = gameState.getMap().getHeight();
-
         final Set<UUID> previousPlayers = moves
                 .stream()
                 .map(move -> move.getPlayer())
@@ -29,7 +26,7 @@ public class Milestone1Bot extends Bot {
             if (player.getOwner().equals(getId())) {
                 playerPositions.add(player.getPosition());
                 if (!previousPlayers.contains(player.getId())) {
-                    moves.add(new TimidMove(mapWidth, mapHeight, player));
+                    moves.add(new TimidMove(gameState.getMap(), player));
                 } else {
                     moves.forEach(move -> {
                         if (move.getPlayer().equals(player.getId())) {

default-bots/src/main/java/com/scottlogic/hackathon/bots/Milestone2Bot.java
@@ -31,9 +31,6 @@ public class Milestone2Bot extends Bot {
             moves.removeIf(move -> move.getPlayer().equals(player.getId()));
         });
 
-        final int mapWidth = gameState.getMap().getWidth();
-        final int mapHeight = gameState.getMap().getHeight();
-
         final Set<UUID> previousPlayers = moves
                 .stream()
                 .map(move -> move.getPlayer())
@@ -52,7 +49,7 @@ public class Milestone2Bot extends Bot {
                     // Determines which bot is selected
                     ProportionalStateSelector proportionalStateSelector = null;
                     try {
-                        proportionalStateSelector = new ProportionalStateSelector(moveCounts, spawnProfile, mapWidth, mapHeight, player);
+                        proportionalStateSelector = new ProportionalStateSelector(moveCounts, spawnProfile, gameState.getMap(), player);
                     } catch (final InvocationTargetException e) {
                         e.printStackTrace();
                     } catch (final NoSuchMethodException e) {
@@ -100,7 +97,7 @@ public class Milestone2Bot extends Bot {
                     .get(0);
             ProportionalTransitionSelector proportionalTransitionSelector = null;
             try {
-                proportionalTransitionSelector = new ProportionalTransitionSelector(moveCounts, transitionProfile, mapWidth, mapHeight, move, movePlayer);
+                proportionalTransitionSelector = new ProportionalTransitionSelector(moveCounts, transitionProfile, gameState.getMap(), move, movePlayer);
             } catch (final InvocationTargetException e) {
                 e.printStackTrace();
             } catch (final NoSuchMethodException e) {

dobanisola-scottlogic/scenario-2-20250908-093933

commit 640e63dc12d38dfc135c1c7cc9fef925ab572427

Fix issue causing mielstone bots to timeout
This problem was caused by a mistake in commit
7fb11e22e7672a8d4c41b47470c05145a335f724, whereby checks to make sure
moves in random directions didn't collide with a set of known player
positions were accidentally inverted. This mistake has been reveresed.

Interestingly it looks like it was possible for these moves to timeout
even under th old behaviour: if the player was completely surrounded by
friendly bots. Although this would have been incredibly unlikely to
occur (17 players would need to move in a very specific pattern to
achieve this situation), this commit also guards against this -
stopping the generation of random directions once all possible
directions have been generated once.

default-bots/src/main/java/com/scottlogic/hackathon/bots/Util.java
@@ -3,10 +3,9 @@ package com.scottlogic.hackathon.bots;
 import com.scottlogic.hackathon.game.Direction;
 import com.scottlogic.hackathon.game.Position;
 
-import java.util.HashMap;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class Util {
 
@@ -114,8 +113,14 @@ public class Util {
         return directions[new Random().nextInt(directions.length)];
     }
 
-    public Stream<Direction> randomDirections() {
-        return Stream.generate(this::randomDirection);
+    public Stream<Direction> preferredThenRandom(Stream<Direction> preferred) {
+        Set<Direction> pending = EnumSet.allOf(Direction.class);
+        return Stream.concat(preferred.peek(pending::remove), StreamSupport.stream(() -> {
+            List<Direction> remaining = new ArrayList<>(pending);
+            Collections.shuffle(remaining);
+            return remaining.spliterator();
+        }, Spliterator.DISTINCT | Spliterator.SIZED | Spliterator.ORDERED | Spliterator.NONNULL,
+                false));
     }
 
     public boolean playersCollideInThisMove(int distance, Direction direction, Position playerPosition, Set<Position> myPlayersPositions) {

default-bots/src/main/java/com/scottlogic/hackathon/bots/move/AttackMove.java
@@ -30,10 +30,10 @@ public class AttackMove extends MoveBase {
             }
             if (nearestOpponentPosition != null) {
                 distance = 0;
-                direction = Stream.concat(getMap().directionsTowards(playerPosition, nearestOpponentPosition), util.randomDirections())
-                        .filter(d -> util.playersCollideInThisMove(2, d, playerPosition, playersPositions))
+                direction = util.preferredThenRandom(getMap().directionsTowards(playerPosition, nearestOpponentPosition))
+                        .filter(d -> !util.playersCollideInThisMove(2, d, playerPosition, playersPositions))
                         .findFirst()
-                        .get(); // Guaranteed to be present
+                        .orElseGet(util::randomDirection);
             } else {
                 setRandomDirectionAndDistance(MINIMUM_RANDOM_DISTANCE, MAXIMUM_RANDOM_DISTANCE);
             }

default-bots/src/main/java/com/scottlogic/hackathon/bots/move/CollectableMove.java
@@ -30,10 +30,10 @@ public class CollectableMove extends MoveBase {
             }
             if (nearestCollectable != null) {
                 distance = 0;
-                direction = Stream.concat(getMap().directionsTowards(playerPosition, nearestCollectable.getPosition()), util.randomDirections())
-                        .filter(d -> util.playersCollideInThisMove(2, d, playerPosition, playersPositions))
+                direction = util.preferredThenRandom(getMap().directionsTowards(playerPosition, nearestCollectable.getPosition()))
+                        .filter(d -> !util.playersCollideInThisMove(2, d, playerPosition, playersPositions))
                         .findFirst()
-                        .get();
+                        .orElseGet(util::randomDirection);
             } else {
                 setRandomDirectionAndDistance(MINIMUM_RANDOM_DISTANCE, MAXIMUM_RANDOM_DISTANCE);
             }

default-bots/src/main/java/com/scottlogic/hackathon/bots/move/HunterMove.java
@@ -44,10 +44,10 @@ public class HunterMove extends MoveBase {
             }
             if (nearestOpponentSpawnPointPosition != null) {
                 distance = 0;
-                direction = Stream.concat(getMap().directionsTowards(playerPosition, nearestOpponentSpawnPointPosition), util.randomDirections())
-                        .filter(d -> util.playersCollideInThisMove(2, d, playerPosition, playersPositions))
+                direction = util.preferredThenRandom(getMap().directionsTowards(playerPosition, nearestOpponentSpawnPointPosition))
+                        .filter(d -> !util.playersCollideInThisMove(2, d, playerPosition, playersPositions))
                         .findFirst()
-                        .get();
+                        .orElseGet(util::randomDirection);
             } else {
                 setRandomDirectionAndDistance(MINIMUM_RANDOM_DISTANCE, MAXIMUM_RANDOM_DISTANCE);
             }

default-bots/src/main/java/com/scottlogic/hackathon/bots/move/MoveBase.java
@@ -48,10 +48,12 @@ public class MoveBase implements Move {
         return map;
     }
 
+    @Override
     public UUID getPlayer() {
         return player;
     }
 
+    @Override
     public Direction getDirection() {
         return direction;
     }

default-bots/src/main/java/com/scottlogic/hackathon/bots/move/TimidMove.java
@@ -30,10 +30,10 @@ public class TimidMove extends MoveBase {
             }
             if (nearestOpponentPosition != null) {
                 distance = 0;
-                direction = Stream.concat(getMap().directionsAwayFrom(playerPosition, nearestOpponentPosition), util.randomDirections())
-                        .filter(d -> util.playersCollideInThisMove(2, d, playerPosition, playersPositions))
+                direction = util.preferredThenRandom(getMap().directionsAwayFrom(playerPosition, nearestOpponentPosition))
+                        .filter(d -> !util.playersCollideInThisMove(2, d, playerPosition, playersPositions))
                         .findFirst()
-                        .get();
+                        .orElseGet(util::randomDirection);
             } else {
                 setRandomDirectionAndDistance(MINIMUM_RANDOM_DISTANCE, MAXIMUM_RANDOM_DISTANCE);
             }